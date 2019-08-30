2e4d7946d6ff0b7ffd8eca02dfba75ff371e5077
ispatel <ispatel@microsoft.com>
2019-08-30 07:08:07 -0700

    Add project files.

---
+++ BestGifts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestGifts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
